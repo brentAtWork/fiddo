65a3c52bbbfd6bac351e3351e948b60c08705c48
Brent Nelson <Brent Nelson>
2017-01-03 23:16:23 -0600

    Adding script running to webhook.

--- fiddoServer.go
+++ fiddoServer.go
@@ -5,9 +5,11 @@ import (
 	"net/http"
 	"log"
 	"encoding/json"
+	"os"
+	"os/exec"
 )
 
-type Commit struct {
+type HeadCommit struct {
 	Id, Url string
 }
 
@@ -17,12 +19,17 @@ type repo struct {
 }
 
 type body_struct struct {
-	Ref string
-	Commits []Commit
-	Repository repo
+	Ref string				`json:"ref"`
+	HeadCommit HeadCommit 	`json:"head_commit"`
+	Repo repo 				`json:"repository"`
 }
 
+var scriptRoot = "/var/fiddoScripts/"
+
 func main() {
+	if (len(os.Args) > 1 && os.Args[1] != "") {
+		scriptRoot = os.Args[1]
+	}
 	http.HandleFunc("/webhook", webhook)
 	http.ListenAndServe(":5000", nil)
 }
@@ -35,7 +42,26 @@ func webhook(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	defer r.Body.Close()
+
+	fullPathScript := scriptRoot + body.Repo.Name + ".sh"
+
+	go executeFiddoScript(fullPathScript, body.HeadCommit.Url)
+
 	log.Printf("Ref be like: %v", body.Ref)
-	log.Printf("Commits be like: %v", body.Commits)
-	log.Printf("Repo be like: %v", body.Repository)
+	log.Printf("Commit be like: %v", body.HeadCommit)
+	log.Printf("Repo be like: %v", body.Repo)
+}
+
+func executeFiddoScript(scriptName string, url string) {
+	if _, err := os.Stat(scriptName); os.IsNotExist(err) {
+		log.Printf("Script not found: %v", scriptName)
+		return
+	}
+	err := exec.Command(scriptName, url).Run()
+	if exitErr, ok := err.(*exec.ExitError); ok {
+		log.Printf("Script exited with error: %v", exitErr.Stderr)
+		return
+	} else if err != nil {
+		panic(err)
+	}
 }
